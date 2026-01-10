275101420e83393dd7065a9104913f1f987b6f03
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-01-10 11:53:21 -0500

    refactor: remove unused button and update h1 title to 'Whiteboard Assistant'

--- Front-End/src/app/page.js
+++ Front-End/src/app/page.js
@@ -3,17 +3,9 @@
 import WhiteboardCanvas from "./components/WhiteBoardCanvas";
 
 export default function Home() {
-   const manageButtonClick = async () => {
-     console.log("Calling Backend");
-     fetch("http://127.0.0.1:5000/process_image")
-     .then((response) => response.json())
-     .then((data) => console.log(data))
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      {/* <button className="border p-4 bg-blue-400 text-white rounded-lg" onClick={manageButtonClick}>Call for the back</button> */}
-      <h1 className="text-4l font-bold mb-8">Whiteboard Drawing</h1>
+      <h1 className="flex text-4l font-bold mb-8">Whiteboard Assistant</h1>
       <WhiteboardCanvas />
     </main>
   );
